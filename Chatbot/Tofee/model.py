9bbb1babb9250b290fea294c9ff9db06de22a588
GarmitPant <garmitpant@gmail.com>
2023-03-31 01:28:02 +0530

    category classifier

--- Chatbot/Tofee/model.py
+++ Chatbot/Tofee/model.py
@@ -1,5 +1,13 @@
 from transformers import pipeline
 
-classifier = pipeline('zero-shot-classification', model='facebook/bart-large-mnli')
-labels = ["entertainment", "food and drink", "home", "lifestyle", "transportation", "utilities"] 
-hypothesis_template = 'This text is about {}.'
+# Classify text using the zero-shot-classification pipeline
+def classify_text(text):
+    classifier = pipeline('zero-shot-classification', model='facebook/bart-large-mnli')
+    labels = ["entertainment", "food and drink", "home", "lifestyle", "transportation", "utilities"] 
+    hypothesis_template = 'This text is about {}.'
+
+    results = classifier(text, labels, hypothesis_template=hypothesis_template)
+    category = results['labels'][0]
+
+    return category
+
