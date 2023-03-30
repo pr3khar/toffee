54421fe16f90c856f06e7ff59e726e8d11bf1ec1
ShrivatsaMishra <55681172+ShrivatsaMishra@users.noreply.github.com>
2023-03-31 03:24:33 +0530

    Added Mongo connectivity

--- Chatbot/Tofee/main.py
+++ Chatbot/Tofee/main.py
@@ -1,6 +1,9 @@
 import logging
+from time import time
+from datetime import datetime
 from telegram.ext import *
 from responses import process_message
+from pymongo import MongoClient
 import re
 
 
@@ -11,8 +14,25 @@ logging.info('Starting Bot...')
 
 names=[]
 
+cluster = MongoClient("mongodb+srv://root:toor@cluster0.8dchtrd.mongodb.net/?retryWrites=true&w=majority")
+db = cluster["toffee"]
+userCollection = db["users"]
+transactionCollection = db["transactions"]
+
 def start_command(update, context):
     update.message.reply_text('Hello there! I\'m a bot. Whats up?')
+    user_id = update.message.from_user.id
+    user_name = update.message.from_user.first_name
+    # print(userCollection.find_one())
+    count=0
+    for x in userCollection.find():
+        if(user_id==x.get("username")):
+            print("Found")
+            count+=1
+    print(x)
+    if(count==0):
+        print("Not found")
+        userCollection.insert_one({"username": user_id, "name": user_name})
 
 def help_command(update, context):
     update.message.reply_text('Try typing anything and I will do my best to respond!')
@@ -23,12 +43,20 @@ def name_command(update, context):
     update.message.reply_text(f'User changed name to {update.message.text}')
 
 def handle_messages(update, context):
+    update.message.reply_text(f'Please wait. Tracking your budget...')
+    user_id = update.message.from_user.id
+    user_name = update.message.from_user.first_name
     text = str(update.message.text).lower()
-    logging.info(f'User ({update.message.from_user.id}) says: {text}')
-    amount, type = process_message(text)
-    logging.info(f'Amount: {amount} \tCategory: {type}')
+    logging.info(f'User ({user_id}) says: {text}')
+    amount, category = process_message(text)
+    if(category == "food and drink"):
+        category="foodAndDrink"
+    logging.info(f'Amount: {amount} \tCategory: {category}')
+
+    transactionCollection.insert_one({"amount": amount, "username": user_id, "category": category, "createdAt": int(time())})
+
     # Bot Response
-    update.message.reply_text(text)
+    update.message.reply_text(f'Amount: {amount} \tCategory: {category}')
 
 def error(update, context):
     # Logs errors
