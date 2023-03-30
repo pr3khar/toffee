8b0ccd67b49cc2354327efd7a980f30539b2d9e9
ShrivatsaMishra <55681172+ShrivatsaMishra@users.noreply.github.com>
2023-03-31 02:00:23 +0530

    Implemented understanding

--- Chatbot/Tofee/main.py
+++ Chatbot/Tofee/main.py
@@ -1,11 +1,15 @@
 import logging
 from telegram.ext import *
-import responses
+from responses import process_message
+import re
+
 
 API_KEY = '6279245879:AAFP_JBtCsXXg1-5MGXQP7cTba4R0uc0K5M'
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logging.info('Starting Bot...')
+
+names=[]
 
 def start_command(update, context):
     update.message.reply_text('Hello there! I\'m a bot. Whats up?')
@@ -13,13 +17,16 @@ def start_command(update, context):
 def help_command(update, context):
     update.message.reply_text('Try typing anything and I will do my best to respond!')
 
-def custom_command(update, context):
-    update.message.reply_text('This is a custom command ')
+def name_command(update, context):
+    names.append(update.message.text)
+    logging.info(f'User changed name to {update.message.text}')
+    update.message.reply_text(f'User changed name to {update.message.text}')
 
-def handdle_messages(update, context):
+def handle_messages(update, context):
     text = str(update.message.text).lower()
-    logging.info('f!User ({update.message.chat.id}) says: {text}')
-
+    logging.info(f'User ({update.message.from_user.id}) says: {text}')
+    amount, type = process_message(text)
+    logging.info(f'Amount: {amount} \tCategory: {type}')
     # Bot Response
     update.message.reply_text(text)
 
@@ -34,10 +41,10 @@ if __name__== '__main__':
     # Commands
     dp.add_handler(CommandHandler('start', start_command))
     dp.add_handler(CommandHandler('help', help_command))
-    dp.add_handler(CommandHandler('custom', custom_command))
+    dp.add_handler(CommandHandler('name', name_command))
 
     # Messages
-    dp.add_handler(MessageHandler(Filters.text, handdle_messages))
+    dp.add_handler(MessageHandler(Filters.text, handle_messages))
 
     # Log all errors
     dp.add_error_handler(error)
--- Chatbot/Tofee/responses.py
+++ Chatbot/Tofee/responses.py
@@ -1,18 +1,27 @@
-import regex
+import re
+from model import classify_text
 
-def process_message(message, response_array, response):
-    list_message = re.findall(r"[w']+|[.,!?;]", message.lower())
-
-    score = 0
-    for word in list_message:
-        if(word in response_array):
-            score = score + 1
-
-    print(score, response)
-    return [score, response]
-
-def get_response(message):
-    response_list = [
-        process_message(message, ['hello', 'hi', 'hey'], 'Hey there!')
+def process_message(message):
+    list_message = message.lower().split()
+    print(list_message)
+    if(len(list_message)<1):
+        return[-1, "No_Message"]
+    
+    amount = list_message[0]
+    if(len(list_message)==1):
+        if(amount.isnumeric()):
+            return [int(amount), 'Misc']
+        else:
+            return[-1, "Not_A_Number"]
+    
+    if(amount.isnumeric()):
+        return [int(amount), classify_text(list_message[1])]
+    else:
+        for x in list_message:
+            if(x.isnumeric()):
+                return [int(x), classify_text(list_message[0])]
+    return[-1, "Not_A_Number"]
         
-    ]
+    
+    
+    
